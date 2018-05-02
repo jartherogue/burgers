676c1b152bed027391728a90d64e8e1228fda51c
jartherogue <jasreev@gmail.com>
2018-05-01 21:29:20 -0600

    trying to get Heroku connection working

--- connection.js
+++ connection.js
@@ -1,11 +1,18 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'burgers_db'
+	})
+};
 
 connection.connect(function(err) {
   if (err) {
